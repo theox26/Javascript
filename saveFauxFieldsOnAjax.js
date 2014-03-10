9491bf3e97a763c8f0f73a2918771b6204d52d48
Aaron <theox26@gmail.com>
2014-03-09 22:42:08 -0400

    use array for proper order

--- saveFauxFieldsOnAjax.js
+++ saveFauxFieldsOnAjax.js
@@ -5,7 +5,9 @@ the StateDropdownObject to be wiped out since the server doesn't know those fiel
 This is a method to save those values to a JSON object and save it in a field then reverse that after
 the AJAX call is processed and the form is repopulated.
 
-Requirements: all.js
+Eventually, I'll mix this in with the main object and make a real oo method out of all of this
+
+Requirements: all.js, I may need to use jquery but will try not to first
 
 ****************/
 
@@ -19,27 +21,30 @@ var fauxFix = {
 	//the number of arguments does not matter
 		var allFields = {};
 		for (var i = 0, j=fauxFix.saveFields.length; i < j; i++) {
-			allFields[fauxFix.saveFields[i]] = FormGetFieldValue(fauxFix.saveFields[i]);
+			var thisValue = FormGetFieldValue(fauxFix.saveFields[i]);
+			if (thisValue !== ''){
+				allFields[fauxFix.saveFields[i]] = FormGetFieldValue(fauxFix.saveFields[i]);
+			}
 		}	
 		fauxFix.JSONfield.value = JSON.stringify(allFields);
 	},
 
 	JSONValuesToFields : function() {
 		var allValues = JSON.parse(fauxFix.JSONfield.value);
-		for (field in allValues) {
+		for (var i = 0, j=fauxFix.saveFields.length; i < j; i++){
 			//we only create dropdowns or checkboxes. This should look at others if you use them
-			var fieldObj = FormGetField(field);
+			var fieldObj = FormGetField(fauxFix.saveFields[i]);
 			if(fieldObj){
 				var fieldType = fieldObj.type ? fieldObj.type.toUpperCase() : "";
 				if(fieldType == "CHECKBOX" || fieldType == "RADIO"){
 					//var valueArray = allValues[field].split("\n");
 					for (box in fieldObj){
-						if(allValues[field].indexOf(fieldObj[box].value) > -1){
+						if(allValues[fauxFix.saveFields[i]].indexOf(fieldObj[box].value) > -1){
 							fieldObj[box].checked = true;
-						}
+						},
 					}
 				} else {
-					FormSetFieldValue(field, allValues[field]);
+					FormSetFieldValue(fauxFix.saveFields[i], allValues[fauxFix.saveFields[i]]);
 				}
 			}
 		}
