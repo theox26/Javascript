c4231cfcd25049259a478b768ffb9826f329f2aa
theox26 <theox26@gmail.com>
2015-02-25 17:03:16 -0500

    accordionTabErrorInfo and fillProducts
    made the accordionTabErrorInfo easier to read and update. Also adding
    start to filling products from other forms.

--- AccordionTabErrorInfo.js
+++ AccordionTabErrorInfo.js
@@ -11,6 +11,50 @@
 <script>
 //postAJAX find the validation divs that are showing. Then find the parent that has the right class
 //use that to find the accordion or tab to highlight with the slValidationError class
+var updateErrorContainers = function() {
+//requires jQuery but if you have tabs or accordion then that is already loaded    
+    var errorTabs = [],
+        tabs = false,
+        $parent,
+        visualErrorFunction = function(){}; //accordion or tabs
+
+    if( $("div#tabs").length ){
+        $("div#tabs li").find("a").removeClass('slValidationErrorTab');
+        tabs = true;
+        visualErrorFunction = function(index){
+            $parent = $(this).parent().parent();
+            if ( $parent.hasClass("ui-tabs-panel") ){
+                errorTabs.push( $parent.attr( "id" ) );
+            } else {
+                //I'm not sure what to do here if anything
+            }
+        };
+    } else if( $("div#accordion").length ){
+        $("div#accordion").find(".ui-accordion-header").removeClass('slValidationErrorField');
+        visualErrorFunction = function(index){
+            $parent = $(this).parent().parent();
+            if ( $parent.hasClass("ui-accordion-content") ){
+                $parent.prev().addClass('slValidationErrorField');
+            } else {
+                //I'm not sure what to do here if anything
+            }
+
+        }
+    } else {
+        return; //there is no enclosure so nothing needs done
+    }
+
+    $("div[id*='VALID_VAR_']").filter(function( index ) {
+        return this.style.display !== "none"; // :visible doesn't work since the accordion hides them
+    }).each(visualErrorFunction);
+
+    if(tabs){
+        for (var i = 0, j= errorTabs.length; i < j; i++) {
+            $("div#tabs").find("li[aria-controls='" + errorTabs[i] + "'] a").addClass('slValidationErrorTab');
+        }
+    }
+}
+
 
 if(window.StorefrontPostAjaxHook){
     //this works to preserve the existing possible functions and add new
@@ -18,95 +62,13 @@ if(window.StorefrontPostAjaxHook){
 
     var StorefrontPostAjaxHook = function(){
         backupExisting();
-
-        //requires jQuery but if you have tabs or accordion then that is already loaded    
-        var errorTabs = [],
-            tabs = false,
-            $parent,
-            visualErrorFunction = function(){}; //accordion or tabs
-
-        if( $("div#tabs").length ){
-            $("div#tabs li").find("a").removeClass('slValidationErrorTab');
-            tabs = true;
-            visualErrorFunction = function(index){
-                $parent = $(this).parent().parent();
-                if ( $parent.hasClass("ui-tabs-panel") ){
-                    errorTabs.push( $parent.attr( "id" ) );
-                } else {
-                    //I'm not sure what to do here if anything
-                }
-            };
-        } else if( $("div#accordion").length ){
-            $("div#accordion").find(".ui-accordion-header").removeClass('slValidationErrorField');
-            visualErrorFunction = function(index){
-                $parent = $(this).parent().parent();
-                if ( $parent.hasClass("ui-accordion-content") ){
-                    $parent.prev().addClass('slValidationErrorField');
-                } else {
-                    //I'm not sure what to do here if anything
-                }
-
-            }
-        } else {
-            return; //there is no enclosure so nothing needs done
-        }
-
-        $("div[id*='VALID_VAR_']").filter(function( index ) {
-            return this.style.display !== "none"; // :visible doesn't work since the accordion hides them
-         }).each(visualErrorFunction);
-
-        if(tabs){
-            for (var i = 0, j= errorTabs.length; i < j; i++) {
-                $("div#tabs").find("li[aria-controls='" + errorTabs[i] + "'] a").addClass('slValidationErrorTab');
-            };
-        }
+        updateErrorContainers();
     }
 
 } else {
 
     var StorefrontPostAjaxHook = function() {
-    //requires jQuery but if you have tabs or accordion then that is already loaded    
-        var errorTabs = [],
-            tabs = false,
-            $parent,
-            visualErrorFunction = function(){}; //accordion or tabs
-
-        if( $("div#tabs").length ){
-            $("div#tabs li").find("a").removeClass('slValidationErrorTab');
-            tabs = true;
-            visualErrorFunction = function(index){
-                $parent = $(this).parent().parent();
-                if ( $parent.hasClass("ui-tabs-panel") ){
-                    errorTabs.push( $parent.attr( "id" ) );
-                } else {
-                    //I'm not sure what to do here if anything
-                }
-            };
-        } else if( $("div#accordion").length ){
-            $("div#accordion").find(".ui-accordion-header").removeClass('slValidationErrorField');
-            visualErrorFunction = function(index){
-                $parent = $(this).parent().parent();
-                if ( $parent.hasClass("ui-accordion-content") ){
-                    $parent.prev().addClass('slValidationErrorField');
-                } else {
-                    //I'm not sure what to do here if anything
-                }
-
-            }
-        } else {
-            return; //there is no enclosure so nothing needs done
-        }
-
-        $("div[id*='VALID_VAR_']").filter(function( index ) {
-            return this.style.display !== "none"; // :visible doesn't work since the accordion hides them
-         }).each(visualErrorFunction);
-
-        if(tabs){
-            for (var i = 0, j= errorTabs.length; i < j; i++) {
-                $("div#tabs").find("li[aria-controls='" + errorTabs[i] + "'] a").addClass('slValidationErrorTab');
-            };
-        }
+        updateErrorContainers();
     }
-
 }
 </script>
---
+++ FillFormFromOtherProduct.js
@@ -0,0 +1,93 @@
+﻿<script type="text/javascript">
+
+function AddCart() {
+
+    var totalprice = 0;
+    // get the user id
+    var user_id = CallSINIMethod("GetValue", ["SystemProperty", "LoggedOnUserID", null]);
+    //This gets the current transaction ID. It may differ from the Doc ID
+    var docID = CallSINIMethod("GetValue", ["SystemProperty", "CurrentUserDocument", user_id]);
+    //this is the products name in the system
+    var docName = CallSINIMethod("GetValue", ["DocumentProperty", "ProductName", docID]);
+
+	// get all documents in Cart
+    var cart = CallSINIMethod("GetListValue", ["UserListProperty", "DocumentsInShoppingCart", user_id]);
+	//Loop through Cart
+    $.each(cart, function () {
+		// Get name of each item
+        var docname = CallSINIMethod("GetValue", ["DocumentProperty", "ProductName", this]);
+		// If no price, return $0.00
+        if (!price) {
+            price = "0.00";
+        }
+		//Print out each item
+        $("#mini_shoppingcart table tbody").append("<tr><td class='tbr'>" + docname + "</td><td> $" + price + "</td></tr>");
+		//Add each item up to find Total Price
+        totalprice += parseFloat(price);
+
+    });
+	//Print out total
+    $("#mini_shoppingcart table tfoot").append("<tr><td class='tbr'>Total</td><td> $" + totalprice + "</td></tr>");
+	$("#total_cart span").html('$' + totalprice);
+	$("#whole_cart").hover(function() {
+			$("#mini_shoppingcart").stop().toggle("slow");
+			});
+}
+
+$(function () {
+    AddCart();
+});
+</script>
+<style type="text/css">
+#mini_shoppingcart {
+display:none;
+background:#fff;
+padding:5px;
+clear:right;
+-webkit-border-radius: 5px;
+-moz-border-radius: 5px;
+border-radius: 5px;
+height:auto;
+}
+#mini_shoppingcart table {
+border-collapse:collapse;
+}
+#mini_shoppingcart td{
+min-width:50px;
+ max-width:300px;
+ padding:5px;
+}
+#mini_shoppingcart tfoot td{
+ font-weight:bold;
+ border-top-style: double;
+border-top-width: 2px;
+border-top-color: black;
+}
+#mini_shoppingcart tr td:first{
+border:left 1px solid black;
+}
+#mini_shoppingcart td.tbr{
+border-right:1px solid black;
+}
+#whole_cart {
+position:absolute;
+right:30px;
+top:20px;
+padding:10px;
+}
+#total_cart{
+cursor:pointer;
+float:right;
+clear:both;
+vertical-align: top;
+font-size: 17px;
+}
+#total_cart span{
+vertical-align:top;
+font-weight:bold;
+padding-left:10px;
+}
+	  </style>
+	  <div id="whole_cart"><a href="/UserContentShoppingCart.aspx"><div id="total_cart"><img src="Custom\Themes\ExceliiAdmin\Inserts\images\icon-cart.png" alt="Shopping"><span></span></div></a>
+	  <div id="mini_shoppingcart"><table><thead><tr><th>Product</th><th>Price</th></tr></thead><tbody></tbody><tfoot></tfoot></table></div>
+</div>
